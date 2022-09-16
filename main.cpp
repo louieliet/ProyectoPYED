aab21da6a481675f131344449904c76fb0963d2f
louieliet <louieliet2003@gmail.com>
2022-09-16 15:03:49 -0500

    Started
    Empecé el proyecto :D

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "Source.h"
+
+using namespace std;
+
+
+int main()
+{
+    
+    User u1("0241718","caca123");
+    Estudiante s1(u1.getIdx(),u1.getID(),"Emiliano");
+
+    vector<User> Users;
+    vector<Estudiante> Students;
+
+    Users.push_back(u1);
+    Students.push_back(s1);
+
+    MakeEmptyUsersFile();
+    MakeUsersFile(Users);
+
+    MakeEmptyStudentsFile();
+    MakeStudentsFile(Students);
+
+
+    Search(Students);
+
+
+}
