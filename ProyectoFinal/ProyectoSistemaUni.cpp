fb64559b655d598d9732a49563998b642a4acdbf
louieliet <louieliet2003@gmail.com>
2022-11-11 00:46:17 -0600

    updated

--- ProyectoFinal/ProyectoSistemaUni.cpp
+++ ProyectoFinal/ProyectoSistemaUni.cpp
@@ -1,11 +1,131 @@
 #include "Listas.h"
 #include "Profesors.h"
-
+#include "WebServer.h"
+#include "Eval.h"
 
 using namespace std;
 
+
+#define KS_Path "C://Temp/wroot/"
+
+string gPath = "";
+
+string fnGetTemplate(string pName)
+{
+    string lContent = "";
+
+    string lPath = KS_Path + pName;
+    ifstream lStream(lPath);
+    stringstream lBuff;
+    lBuff << lStream.rdbuf();
+    lContent = lBuff.str();
+
+    return lContent;
+} // fnGetTemplate
+
+strVec fnGetPath(string pCaller)
+{
+    strVec lParts;
+    strVec lSects;
+    strVec lPath;
+
+    strSplit(pCaller, '?', lParts);
+    if (lParts.size() == 0)
+        lParts.push_back(pCaller);
+    if (lParts.size() > 0) {
+        strSplit(lParts[0], ':', lSects);
+        if (lSects.size() >= 3)
+            strSplit(lSects[2], '/', lPath);
+    }
+
+    return lPath;
+} // fnGetPath
+
+map<string, string> fnGetParams(string pCaller)
+{
+    strVec lParts;
+    strVec lPrms;
+    strVec lItem;
+    map<string, string> lParams;
+
+    strSplit(pCaller, '?', lParts);
+    if (lParts.size() == 2) {
+        strSplit(lParts[1], '&', lPrms);
+        if (lPrms.size() > 0) {
+            for (int lIdx = 0; lIdx < lPrms.size(); lIdx++) {
+                strSplit(lPrms[lIdx], '=', lItem);
+                if (lItem.size() == 2)
+                    lParams.insert(pair<string, string>(lItem[0], lItem[1]));
+                else
+                    lParams.insert(pair<string, string>(lItem[0], ""));
+                lItem.clear();
+            }
+        }
+    }
+
+    return lParams;
+} // fnGetParams
+
+aClase sob = aClase();
+
+string showAlumnos(string pCaller) {
+
+    string lRes = fnGetTemplate("lalumno.html");
+    string lHTML = "";
+    PDNODO2 lData = NULL;
+    int lRow = 0;
+    string lColor = "";
+    while (lData = sob.alumn.get()) {
+        lColor = "";
+        if (lRow % 2 == 0)
+            lColor = "background-color:#E0E0E0;";
+        lHTML += "<tr " + lColor + "'>\n";
+        lHTML += "<td>\n" + lData->sIDP + "</td>";
+        lHTML += "<td>\n" + lData->sNombre + "</td>";
+        lHTML += "<td>\n" + lData->sCorreo + "</td>";
+        lHTML += "</tr>";
+        lRow++;
+    }
+
+    strReplace(lRes, "{{pData}}", lHTML);
+
+    return lRes;
+
+}
+
+string showProfesores(string pCaller) {
+
+    string lRes = fnGetTemplate("lprofesor.html");
+    string lHTML = "";
+    PDNODO lData = NULL;
+    int lRow = 0;
+    string lColor = "";
+    while (lData = sob.prof.get()) {
+        lColor = "";
+        if (lRow % 2 == 0)
+            lColor = "background-color:#E0E0E0;";
+        lHTML += "<tr " + lColor + "'>\n";
+        lHTML += "<td>\n" + lData->sIDP + "</td>";
+        lHTML += "<td>\n" + lData->sNombre + "</td>";
+        lHTML += "<td>\n" + lData->sCorreo + "</td>";
+        lHTML += "</tr>";
+        lRow++;
+    }
+
+    strReplace(lRes, "{{pData}}", lHTML);
+
+    return lRes;
+
+}
+
+
 int main() {
-    aClase sob = aClase();
+
+    MyHTTPSrv lSrv = MyHTTPSrv(NULL, NULL);
+    //lSrv.SetGetFunc(&fnGetFuncS);
+    //lSrv.Run();
+
+
     int opci, subs = 1;
     //sob.admi.InsertarAdmin();
     sob.admi.read("Admin.csv");
@@ -66,10 +186,10 @@ int main() {
 
 
                         case 5: sob.prof.InsertarProfe(); break;
-                        case 6: sob.prof.repr(); break;
+                        case 6: lSrv.SetGetFunc(&showProfesores); lSrv.Run(); break;
 
                         case 7:sob.alumn.InsertarAlumno(); break;
-                        case 8:sob.alumn.repr(); break;
+                        case 8:lSrv.SetGetFunc(&showAlumnos); lSrv.Run(); break;
 
                         case 9:sob.admi.InsertarAdmin(); break;
                         case 10:sob.admi.repr(); break;
@@ -146,7 +266,6 @@ int main() {
                 }while(subs!=0);
             }
         }
-        
 
     } while (opci != 0);
     
